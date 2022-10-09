41748f9f8aa415b2a9d3cc396fcbb99121c3480b
SIDs1de <igorekdenisenkov@yandex.ru>
2022-10-10 00:46:36 +0300

    сделал модальные окна

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html');
+  const body = document.querySelector('body');
+
+  const animate = () => {
+    if (window.innerWidth <= 1200) {
+      const inst = document.querySelector('.insta');
+
+      if (inst.classList.contains('animate__fadeInRight')) {
+        inst.classList.remove('animate__fadeInRight');
+        inst.classList.add('animate__fadeIn');
+      }
+    }
+
+    const titles = document.querySelectorAll('.title');
+
+    titles.forEach((title) => {
+      title.classList.add('wow', 'animate__animated', 'animate__fadeInUp');
+    });
+  };
+  
+  animate();
+
   new WOW().init();
 
   const whoSlider = new Swiper('.swiper-who', {
@@ -193,6 +215,198 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   });
 
+  const ratesSlider = new Swiper('.swiper-rates', {
+    // Свои классы
+    wrapperClass: 'swiper-rates__wrapper',
+    slideClass: 'swiper-rates__item',
+    spaceBetween: 20,
+
+    // Вертикальный слайдер
+    // direction: 'vertical',
+
+    // Количество слайдов для показа
+    slidesPerView: 'auto',
+
+    enabled: true,
+
+    grabCursor: true,
+
+    // Включаем параллакс
+    // parallax: true,
+
+    // Управление клавиатурой
+    keyboard: {
+      // Включить\выключить
+      enabled: true,
+      // Включить\выключить только когда слайдер в пределах вьюпорта
+      onlyInViewport: true,
+      // Включить\выключить управление клавишами pageUp, pageDown
+      pageUpDown: true,
+    },
+
+    // Управление колесом мыши
+    // mousewheel: {
+    //   // Чувствительность колеса мыши
+    //   sensitivity: 1,
+    //   // Класс объекта, на котором будет
+    //   // срабатывать прокрутка мышью
+    //   // events.target: '.image-slider'
+    // },
+
+    // Отключение функционала
+    // при изменении элементов слайдера
+    watchOverflow: true,
+
+    // Скорость
+    speed: 800,
+
+    // Обновить свайпер
+    // при изменении родительских
+    // элементов слайдера
+    observeParents: true,
+
+    // Обновить свайпер
+    // при изменении дочерних
+    // элементов слайда
+    observeSlideChildren: true,
+
+    // Навигация
+    // Буллеты, текущее положение, прогрессбар
+    pagination: {
+      el: '.swiper-rates__pagination',
+      type: 'bullets',
+      clickable: true,
+      bulletClass: 'swiper-rates__bullet',
+      bulletActiveClass: 'swiper-rates__bullet--active',
+    },
+    // Скролл
+    // scrollbar: {
+    //   el: '.page__scroll',
+    //   dragClass: 'page__drag-scroll',
+    //   // Возможность перетаскивать скролл
+    //   draggable: true,
+    // },
+
+    // Отключаем автоинициализацию
+    // init: false,
+
+    // События
+    // on: {
+    //   // Событие инициализации
+    //   init: function () {
+    //     menuSlider();
+    //     setScrollType();
+    //     wrapper.classList.add('_loaded');
+    //   },
+    //   // Событие смены слайда
+    //   slideChange: function () {},
+
+    //   resize: function () {},
+    // },
+
+    breakpoints: {
+      1001: {
+        slidesPerView: 2,
+      },
+    },
+  });
+
+  const popupSlider = new Swiper('.swiper-popup', {
+    // Свои классы
+    wrapperClass: 'swiper-popup__wrapper',
+    slideClass: 'swiper-popup__item',
+    spaceBetween: 20,
+
+    // Вертикальный слайдер
+    // direction: 'vertical',
+
+    // Количество слайдов для показа
+    slidesPerView: 'auto',
+
+    enabled: true,
+
+    grabCursor: true,
+
+    // Включаем параллакс
+    // parallax: true,
+
+    // Управление клавиатурой
+    keyboard: {
+      // Включить\выключить
+      enabled: true,
+      // Включить\выключить только когда слайдер в пределах вьюпорта
+      onlyInViewport: true,
+      // Включить\выключить управление клавишами pageUp, pageDown
+      pageUpDown: true,
+    },
+
+    // Управление колесом мыши
+    // mousewheel: {
+    //   // Чувствительность колеса мыши
+    //   sensitivity: 1,
+    //   // Класс объекта, на котором будет
+    //   // срабатывать прокрутка мышью
+    //   // events.target: '.image-slider'
+    // },
+
+    // Отключение функционала
+    // при изменении элементов слайдера
+    watchOverflow: true,
+
+    // Скорость
+    speed: 800,
+
+    // Обновить свайпер
+    // при изменении родительских
+    // элементов слайдера
+    observeParents: true,
+
+    // Обновить свайпер
+    // при изменении дочерних
+    // элементов слайда
+    observeSlideChildren: true,
+
+    // Навигация
+    // Буллеты, текущее положение, прогрессбар
+    pagination: {
+      el: '.swiper-popup__pagination',
+      type: 'bullets',
+      clickable: true,
+      bulletClass: 'swiper-popup__bullet',
+      bulletActiveClass: 'swiper-popup__bullet--active',
+    },
+    // Скролл
+    // scrollbar: {
+    //   el: '.page__scroll',
+    //   dragClass: 'page__drag-scroll',
+    //   // Возможность перетаскивать скролл
+    //   draggable: true,
+    // },
+
+    // Отключаем автоинициализацию
+    // init: false,
+
+    // События
+    // on: {
+    //   // Событие инициализации
+    //   init: function () {
+    //     menuSlider();
+    //     setScrollType();
+    //     wrapper.classList.add('_loaded');
+    //   },
+    //   // Событие смены слайда
+    //   slideChange: function () {},
+
+    //   resize: function () {},
+    // },
+
+    breakpoints: {
+      1001: {
+        slidesPerView: 2,
+      },
+    },
+  });
+
   const swiperActive = () => {
     if (window.innerWidth <= 1000) {
       whoSlider.enabled = true;
@@ -228,26 +442,46 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   };
 
-  const animate = () => {
-    if (window.innerWidth <= 1200) {
-      const inst = document.querySelector('.insta');
+  const popupOpenBtns = document.querySelectorAll('[data-open-pop]');
+  const popups = document.querySelectorAll('[data-pop]');
+  const headerBg = document.querySelector('.header__bg');
+  const popupBgs = document.querySelectorAll('.popup__bg');
 
-      if (inst.classList.contains('animate__fadeInRight')) {
-        inst.classList.remove('animate__fadeInRight');
-        inst.classList.add('animate__fadeIn');
-      }
-    }
-
-    const titles = document.querySelectorAll('.title');
-
-    titles.forEach((title) => {
-      title.classList.add('wow', 'animate__animated', 'animate__fadeInUp');
+  const openPopup = () => {
+    popupOpenBtns.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        html.classList.add('lock');
+        popups.forEach((popup) => {
+          if (popup.dataset.pop == e.target.dataset.openPop) {
+            popup.classList.add('active');
+          }
+        });
+        e.preventDefault();
+      });
+    });
+    popupBgs.forEach((bg) => {
+      bg.addEventListener('click', (e) => {
+        html.classList.remove('lock');
+        let pop = document.querySelector(
+          `[data-pop="${e.target.dataset.close}"]`
+        );
+        pop.classList.remove('active');
+      });
+    });
+    document.querySelectorAll('[data-close]').forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        let pop = document.querySelector(
+          `[data-pop="${e.target.dataset.close}"]`
+        );
+        pop.classList.remove('active');
+        html.classList.remove('lock');
+      });
     });
   };
 
   window.addEventListener('resize', swiperActive);
-  animate();
   window.addEventListener('resize', animate);
+  openPopup();
   swiperActive();
   questions();
 });
